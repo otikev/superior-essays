d015f80fe4b70b4c112d32d3b6d00bec9037c9ca
kevin <test@mail.com>
2021-04-13 02:18:03 +0300

    Fix datatables

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,16 +3,21 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import 'bootstrap/dist/js/bootstrap'
+
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require('datatables.net-bs4')
-require("custom/app")
-require("custom/custom")
 
 import $ from 'jquery';
 global.$ = jQuery;
+window.Rails = Rails
+
+import 'bootstrap';
+import 'popper.js/dist/popper.js';
+
+require("custom/app")
+require("custom/custom")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,6 +5,7 @@ environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
+    Rails: '@rails/ujs',
     Popper: ['popper.js','default']
   })
 )

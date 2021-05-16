37772f1de85dbb276f04b255012848daa839c1e7
kevin <oti.kevin@gmail.com>
2021-05-16 23:05:58 +0300

    disable wip

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -81,7 +81,7 @@ class OrdersController < ApplicationController
   def show
     ky = params[:key]
     @order = Order.where(key:ky).first
-    @order.remaining_minutes
+    #@order.remaining_minutes
     @resource = Resource.new
 
     if !@order # order not found!

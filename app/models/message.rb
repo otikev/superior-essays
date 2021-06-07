7a1aa2737dc2c3efdbfa08383afd0f0b19bf2961
Kevin Otieno <oti.kevin@gmail.com>
2021-06-07 22:55:35 +0300

    Show oldest unread messages first in the unread messages screen

--- app/models/message.rb
+++ app/models/message.rb
@@ -37,7 +37,7 @@ class Message < ApplicationRecord
 
   def self.unread_for_user(user)
     #TODO: Cache this result to avoid unnecessarilly hitting the DB
-    unread_messages = Message.find_by_sql("SELECT * FROM messages where user_id != #{user.id} and id not in (select message_id from read_messages where user_id = #{user.id}) order by id desc")
+    unread_messages = Message.find_by_sql("SELECT * FROM messages where user_id != #{user.id} and id not in (select message_id from read_messages where user_id = #{user.id}) order by id asc")
   end
 
   def mark_as_read_for_user(user)

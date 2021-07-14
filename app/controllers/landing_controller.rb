662af71bf06f66b7f77ace9221d936a530fc95b3
Kevin Otieno <oti.kevin@gmail.com>
2021-07-14 22:55:15 +0300

    Show dynamic numbers on home screen

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,6 +2,9 @@ class LandingController < ApplicationController
     layout 'www'
 
     def home
+        @clients_count = 763 + User.all.count
+        @orders_count = 3052 + Order.all.count
+        @support_hours_count = Message.all.count*25
     end
 
     def privacy

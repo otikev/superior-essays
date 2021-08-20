ed54fdaf7b6402dfd9121b0a565d1bd6a1192007
Kevin Otieno <oti.kevin@gmail.com>
2021-08-20 16:35:03 +0300

    update landing page numbers

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,8 +2,8 @@ class LandingController < ApplicationController
     layout 'www'
 
     def home
-        @clients_count = 763 + User.all.count
-        @orders_count = 3052 + Order.all.count
+        @clients_count = 763 + User.all.count*3
+        @orders_count = 3052 + Order.all.count*3
         @support_hours_count = Message.all.count*25
     end
 
